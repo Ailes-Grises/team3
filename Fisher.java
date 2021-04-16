371bd62fde2c7a3a23846177fc793ed1edc209da
Ailes-Grises <toriniku.n.s@gmail.com>
2021-04-16 17:06:16 +0900

    一応の実装までは完了

--- Fisher.java
+++ Fisher.java
@@ -11,17 +11,31 @@ public class Fisher {
 	 */
 
 	private String name;
-	private ArrayList<String> fished_list;
 	private int sum;
+	private ArrayList<String> fished_list;
 
 	public Fisher(){
+		this.name = null;
+		this.sum = 0;
 	}
 	public Fisher(String name){
 		this.name = name;
+		this.sum = 0;
 	}
 
-	public void calc_sum(Spot spot){
-		this.sum += spot.getPoint("マグロ");
+	public void calc_sum(Spot spot, String fish_name){
+		this.sum += spot.getPoint(fish_name);
+	}
+	public void fishing(Spot spot){
+		int rand = (int)Math.random()*7;
+		String [] fish = {"マグロ", "メダカ", "タコ", "長靴", "はまち", "ヒラメ", "スカ"};
+		// this.calc_sum(spot, fish[(int)Math.random()*7]);
+		System.out.println(rand + "\n");
+		this.calc_sum(spot, fish[rand]);
+		this.toString();
+	}
+	public String toString(){
+		return "ポイント : " + (int)this.sum;
 	}
 
 }
--- Main.java
+++ Main.java
@@ -4,8 +4,20 @@ import java.util.*;
 public class Main {
 
 	public static void main(String[] args) {
+		// Spot [] spot = new Spot()[5];
 		Spot spot = new Spot();
-		System.out.println(spot.getPoint("マグロ"));
+		Fisher fisher = new Fisher("池田さん");
+
+		System.out.println("どこで釣りますか? > ");
+		Scanner scan = new Scanner(System.in);
+		String select = scan.nextLine();
+
+		for(int i=0; i < 3; i++){
+			fisher.fishing(spot);
+			System.out.println(fisher.toString());
+		}
+		scan.close();
+
 
 	}
 
