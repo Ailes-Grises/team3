338bb943a53af4d192e1f21a6be61b5f08b3278d
Ailes-Grises <toriniku.n.s@gmail.com>
2021-04-16 18:27:25 +0900

    fish_list を整えた。表示もきれいにした。

--- Fisher.java
+++ Fisher.java
@@ -19,15 +19,29 @@ public class Fisher {
 	public void calc_sum(Spot spot, String fish_name){
 		this.sum += spot.getPoint(fish_name);
 	}
+
 	public void fishing(Spot spot){
-		int rand = (int)(Math.random()*7);
-		String [] fish = {"マグロ", "メダカ", "タコ", "長靴", "はまち", "ヒラメ", "スカ"};
-		// this.calc_sum(spot, fish[(int)Math.random()*7]);
-		this.calc_sum(spot, fish[rand]);
+		// 旧バージョンの書き方(廃止)
+		// int rand = (int)(Math.random()*7);
+		// String [] fish = {"マグロ", "メダカ", "タコ", "長靴", "はまち", "ヒラメ", "スカ"};
+		// this.calc_sum(spot, fish[(int)(Math.random()*7)]);
+		// this.calc_sum(spot, fish[rand]);
+		// this.fished_list.add(fish[rand]);
+		// this.toString();
+
+		int rand = (int)(Math.random()*spot.getFish_list_size());
+		this.calc_sum(spot, spot.getFish_list_name(rand));
+		this.fished_list.add(spot.getFish_list_name(rand));
 		this.toString();
 	}
+
 	public String toString(){
-		return "ポイント : " + (int)this.sum;
+		String msg = "<これまでに釣れた魚>\n";
+		for(int i = 0; i < this.fished_list.size(); i++){
+			 msg += this.fished_list.get(i) + "\n";
+		}
+		msg += "ポイント : " + (int)this.sum;
+		return msg;
 	}
 
 }
--- Spot.java
+++ Spot.java
@@ -4,30 +4,59 @@ import java.util.HashMap;
 
 public class Spot {
 
-	private HashMap<String, Integer> fish = new HashMap<String, Integer>();
-	private ArrayList<String> fished_list;
+	private HashMap<String, Integer> fish_hash = new HashMap<String, Integer>();
+	private ArrayList<String> fish_list = new ArrayList<String>();
 
 	public Spot(){
-		this.fish.put("マグロ", 100);
-		this.fish.put("メダカ", 1);
-		this.fish.put("タコ", 30);
-		this.fish.put("長靴", -30);
-		this.fish.put("はまち", 70);
-		this.fish.put("ヒラメ", 50);
-		this.fish.put("スカ", 0);
+		// for fish_hash
+		this.fish_hash.put("マグロ", 100);
+		this.fish_hash.put("メダカ", 1);
+		this.fish_hash.put("タコ", 30);
+		this.fish_hash.put("長靴", -30);
+		this.fish_hash.put("はまち", 70);
+		this.fish_hash.put("ヒラメ", 50);
+		this.fish_hash.put("スカ", 0);
+
+		// for fish_list
+		this.fish_list.add("マグロ");
+		this.fish_list.add("メダカ");
+		this.fish_list.add("タコ");
+		this.fish_list.add("長靴");
+		this.fish_list.add("はまち");
+		this.fish_list.add("ヒラメ");
+		this.fish_list.add("スカ");
+
 	}
 	public Spot(int temp){
-		this.fish.put("マグロ", 100);
-		this.fish.put("メダカ", 1);
-		this.fish.put("タコ", 30);
-		this.fish.put("長靴", -30);
-		this.fish.put("はまち", 70);
-		this.fish.put("ヒラメ", 50);
-		this.fish.put("スカ", 0);
+		this.fish_hash.put("マグロ", 100);
+		this.fish_hash.put("メダカ", 1);
+		this.fish_hash.put("タコ", 30);
+		this.fish_hash.put("長靴", -30);
+		this.fish_hash.put("はまち", 70);
+		this.fish_hash.put("ヒラメ", 50);
+		this.fish_hash.put("スカ", 0);
+
+		// for fish_list
+		this.fish_list.add("マグロ");
+		this.fish_list.add("メダカ");
+		this.fish_list.add("タコ");
+		this.fish_list.add("長靴");
+		this.fish_list.add("はまち");
+		this.fish_list.add("ヒラメ");
+		this.fish_list.add("スカ");
 	}
+
 	public Integer getPoint(String key){
-		return this.fish.get(key);
+		return this.fish_hash.get(key);
 	}
 
-
+	public void fish_list_update(String fish){
+		this.fish_list.remove(this.fish_list.indexOf(fish));
+	}
+	public int getFish_list_size(){
+		return this.fish_list.size();
+	}
+	public String getFish_list_name(int seq){
+		return this.fish_list.get(seq);
+	}
 }
